4bf9d987cb6c15e87c8675f692f3203f1ffb024b
talia wolf <taliamwolf@gmail.com>
2020-06-14 15:50:24 +0300

    adding new and index pages

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'webpacker'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 gem 'cloudinary', '~> 1.12.0'
+gem 'simple_form'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,28 @@
 class PlantsController < ApplicationController
+  def index
+    @plants = Plant.all
+  end
+
+  def show
+    @plant = Plant.find(params[:id])
+  end
+
+  def new
+    @plant = Plant.new
+  end
+
+  def create
+    @plant = Plant.new(plant_params)
+    @plant.save
+    if @plant.save
+      redirect_to plant_path(@plant), notice: 'added!'
+    else
+      render :new
+    end
+  end
+
+  private
+  def plant_params
+    params.require(:plant).permit(:title, :description, :photo)
+  end
 end
